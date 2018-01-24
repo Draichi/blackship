5d6f67312ce1bd61ddba0b33ae4da8a97c57e220
Draichi <lucasdraichi@gmail.com>
2018-01-24 02:01:41 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      fixed
+      :mini-variant="miniVariant"
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app class="primary white--text">
+      <v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        items: [
+          {icon: 'bubble_chart', title: 'Inspire'}
+        ],
+        miniVariant: true,
+        title: 'Blackship'
+      }
+    }
+  }
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 sm10 offset-sm1>
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step step="1" :complete="e1 > 1">Country</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="2" :complete="e1 > 2">Weight</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="3">Size</v-stepper-step>
+          </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-layout>
+                <v-flex>
+                  <div class="display-2">Enter your destination</div>
+                  <v-select
+                    v-bind:items="selectItems"
+                    v-model="selectEl"
+                    label="Country"
+                    single-line
+                    bottom
+                  ></v-select>
+                </v-flex>
+              </v-layout>
+              <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
+              <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="2">
+              <v-card height="350px">
+                <div class="display-2">Weight</div>
+                <v-form v-model="valid">
+                  <v-text-field
+                    label="Grams"
+                    v-model="weight"
+                    required
+                  ></v-text-field>
+                </v-form>
+                <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+              </v-card>
+            </v-stepper-content>
+            <v-stepper-content step="3">
+              <v-layout>
+                <v-flex xs12>
+                  <div class="display-2">Size</div>
+                  <v-form v-model="valid">
+                    <v-text-field
+                      label="Length (cm)"
+                      v-model="length"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="Width (cm)"
+                      v-model="width"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="Height (cm)"
+                      v-model="height"
+                      required
+                    ></v-text-field>
+                  </v-form>
+                </v-flex>
+              </v-layout>
+              <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
+              <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        selectItems: [
+          { text: 'Oi' },
+          { text: 'Oiii' },
+          { text: 'Oiiii' },
+          { text: 'Oiiiii' }
+        ],
+        selectEl: null,
+        e1: 0,
+        valid: false,
+        weight: '',
+        length: '',
+        width: '',
+        height: '',
+        nameRules: [
+          (v) => !!v || 'Name is required',
+          (v) => v.length <= 10 || 'Name must be less than 10 characters'
+        ],
+        emailRules: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        ]
+      }
+    }
+  }
+</script>
